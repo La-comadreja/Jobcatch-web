8126adf29c8fc651b7b10ac0fd74c746ec771181
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-06-22 18:59:02 -0400

    Changed to empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobcatchWeb::Application.config.secret_token = '1c11a65c6243714e3fd4f5a435655958d83c1faefcb9bcc14adacce6d1955af4ac5ab37ea1d7c17c962ee16f72d25361dcc2aab80ee1666ca275c8286c9cbbb7'

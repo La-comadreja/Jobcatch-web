86542f2422eb365404d5a4b12fe437dedbc8ddca
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-08-11 09:38:29 -0400

    Fix type of URL in schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(:version => 20130810171109) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "title"
-    t.string   "url"
+    t.text     "url"
   end
 
   create_table "likes", :force => true do |t|

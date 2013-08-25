db65a3fbfa30971d5d066d2e4045eb0e27dc76e8
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-08-24 22:52:56 -0400

    Add parameters for User information

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ActiveRecord::Base
 
   has_many :likes, :class_name => 'Like', dependent: :destroy
 
+  validates :email, :presence => true, :length => { :minimum => 5, :maximum => 100 }
+  validates :name, :presence => true, :length => { :maximum => 100 }
+  validates :title, :presence => true, :length => { :maximum => 150 }
+  validates :information, :length => { :maximum => 600 }
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :title, :information
-  # attr_accessible :title, :body
 end
